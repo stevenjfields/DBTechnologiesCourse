3284e0a7d9110ecd457f49c06ce8fedaec5e4c9b
Steven Fields <stevenjfields97@gmail.com>
2022-07-29 14:38:18 -0500

    updated requirements

--- database_course/settings.py
+++ database_course/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@w3gnp6fd*g2^!7hrimicn0jcm3yqmbm@x_y)%k!e*w$nw$m3p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["test-django-sfields.herokuapp.com", "127.0.0.1"]
 
